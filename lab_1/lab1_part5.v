caed3dc95794166bda7d60f67c83c673611a85c4
Quyet Luu <huuquyet151091@gmail.com>
2016-07-26 21:51:36 +0700

    Lab1: Correct function part 5

--- lab_1/lab1_part5.v
+++ lab_1/lab1_part5.v
@@ -1,10 +1,18 @@
-module lab1_part5(SW,HEX0);
+/*
+	Author: Quyet Luu
+	Date: 7/25/2016
+*/
+module lab1_part5(SW,LEDR,HEX0);
 input[9:0] SW;
+output [9:0] LEDR;
 output reg [6:0] HEX0;
 wire [1:0] connecttion;
 
 assign connecttion = SW[9]? SW[1:0] : 
 				  SW[8]? SW[3:2] : SW[5:4];
+				  
+assign LEDR = SW[5:0] & ( 10'b110000 >> SW[9:8] >> SW[9:8] );
+
 always @ (connecttion)
 begin
 	case(connecttion)
